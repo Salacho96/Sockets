e2b933df4b6117dd30c53b1a227100b69982cb06
dbook-1629 <dbook-1629@outlook.com>
2023-03-08 19:12:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 
 function App() {
+
+  const socket = io();
+
+  const [sock,setSock] =useState('')
+
+  const url = 'wss://6wpovdvzcg.execute-api.us-east-1.amazonaws.com/production';
+
+  // const url2 = 'https://6wpovdvzcg.execute-api.us-east-1.amazonaws.com/production'
+
+  var jsonData = JSON.stringify({ action : 'set_as_restaurant' ,restaurant_name:'test1',restaurant_location_id: 'id1'})
+
+
+  function setConnection() {
+    socket.current = new WebSocket(url);
+    socket.current.onopen = () => {
+      socket.current.send(jsonData)
+      alert("Conexion exitosa!")
+      // Listen for any event
+      socket.current.onmessage = (message) => {
+        console.log(message);
+      };
+    };
+    socket.current.onclose = () => alert("socket closed");
+    return () => {
+      socket.current.close();
+    };
+  }
+
+  socket.onAny('event',(data) => {
+    console.log("entro")
+    console.log('Received data:', data);
+  }); 
+
+  function recibirMensaje(){
+    socket.onmessage = (message) => {
+      console.log(message);
+    };
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div onClick={setConnection} style={{
+        textAlign: 'center',
+        width: '100px',
+        border: '1px solid gray',
+        borderRadius: '5px'
+      }}>
+        send restaurant
+      </div>
+
+      <div onClick={recibirMensaje} style={{
+        textAlign: 'center',
+        width: '100px',
+        border: '1px solid gray',
+        borderRadius: '5px'
+      }}>
+        Recibir Mensage
+      </div>
+
+      
+    </>
   );
+  
 }
 
-export default App;
+
+export default App; 
